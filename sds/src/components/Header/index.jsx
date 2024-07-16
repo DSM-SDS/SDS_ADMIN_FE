7f96f84b5747d2a97bb9704f1b5692092a8a4ad4
downpour07 <choiheeyoung10@gmail.com>
2024-07-16 17:26:54 +0900

    📑 :: Header 퍼블리싱

--- sds/src/components/Header/index.jsx
+++ sds/src/components/Header/index.jsx
@@ -3,18 +3,31 @@ import axios from "axios";
 import { styled } from "styled-components";
 import { color } from "../../styles/theme";
 
+import Logo from "../../assets/icon/Logo";
+
 function Header() {
 
   return (
-    <Container></Container>
+    <Container>
+      <LogoStyle>
+        <Logo />
+      </LogoStyle>
+    </Container>
   );
 }
 
 const Container = styled.div`
 	width: 100%;
 	height: 6vh;
-    background-color: ${color.White};
+  background-color: ${color.White};
+  display: flex;
+  align-items: center;
 `;
+
+const LogoStyle = styled.div`
+  margin-left: 350px;
+  display: flex;
+`
 
 
 export default Header;
